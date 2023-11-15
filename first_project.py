45253b6c8da52d680cf78fefe65d05d50c6c2421
BadBoySonic <150901959+BadBoySonic@users.noreply.github.com>
2023-11-15 14:55:25 +0530

    Add files via upload

---
+++ first_project.py
@@ -0,0 +1,22 @@
+print('''
+
+                                                                                 __ 
+ __ __                _____                 _            _____     _           _|  |
+|  |  |___ _ _ ___   |  _  |___ ___ ___ _ _|_|___ ___   |   __|___|_|___ ___ _| |  |
+|_   _| . | | |  _|  |     |   |   | . | | | |   | . |  |   __|  _| | -_|   | . |__|
+  |_| |___|___|_|    |__|__|_|_|_|_|___|_  |_|_|_|_  |  |__|  |_| |_|___|_|_|___|__|
+                                       |___|     |___|                              
+''')
+from time import sleep
+
+
+Question = input("Have You Ever Thought Why We Exist?\n")
+print(Question +"But Why?")
+Question2 = input("Have You Ever Thought W H A T Is Beyond The Universe?\n")
+print("Ok....") ; sleep(1)
+print("Maybe There Are Flying Poops And Air Toilets Somewhere In This Universe.") ; sleep(2.5)
+print("Right?") ; sleep(1)
+print("RIGHT?") ; sleep(1)
+print("Ok Then.... Bye!") ; sleep(10)
+print("LOSER!")
+    
